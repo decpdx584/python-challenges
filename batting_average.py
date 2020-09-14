8d87c8fcee53034f45365b870307c6e69f5469b0
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-09-14 15:32:31 -0700

    [battingAverage] Add battingAverage class and methods

---
+++ batting_average.py
@@ -0,0 +1,20 @@
+class BattingAverage():
+    def __init__(self, atBats, strikeOuts, hits):
+        self.atBats = 0
+        self.strikeOuts = 0
+        self.hits = 0
+
+    def atThePlate(atBats):
+        self.atBats += 1
+        print("Your average is: " + {self.hits}/{self.atBats})
+
+    def strikeOut(atBats, strikeOuts):
+        self.atBats += 1
+        self.strikeOuts += 1
+        print("Your average is: " + {self.hits}/{self.atBats})
+
+
+    def getAHit(atBats, hits):
+        self.atBats += 1
+        self.hits += 1
+        print("Your average is: " + {self.hits}/{self.atBats})

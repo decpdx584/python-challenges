28800188001f138934b8cced3051bce31e5a9649
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-09-14 15:17:37 -0700

    [bank_account] Copy Rome's code for bank account

--- bank_account.py
+++ bank_account.py
@@ -1,15 +1,50 @@
 class BankAccount():
-    def __init__(self, kind):
+    def __init__(self, kind, pin):
         self.kind = kind
         self.balance = 0
         self.overdraft_fees = 0
-
+        self.pin = pin
+    
     def deposit(self, amount):
-        self.balance = += amount
+        self.balance += amount
 
-    def withdraw(self, amount):
-        self.balance = -= amount
 
-        if (self.balance < 0):
-            self.overdraft_fees += 36
-            return amount
+    def withdraw(self, amount, pin_from_user):
+
+        if (self.pin == pin_from_user):
+            if (self.balance < amount):
+                print("Sorry, you don't have that amount in your account.")
+                return
+            elif (self.balance == amount):
+                self.balance -= amount
+                print("You are now empty my friend AKA you're broke.")
+                return
+            else:
+                self.balance -= amount
+                print("Current balance is now {}".format(self.balance))
+                return 
+        else:
+            print("The pin number is incorrect. Please try again")
+        # if (self.balance < 0):
+        #     self.overdraft_fees += 36
+        # return amount
+
+    def change_pin(self, pin):
+        self.pin = pin
+        print(f"The new pin number is {self.pin}")
+
+
+cabassa_checking = BankAccount("checking", 1234)
+print("My new account is a {} account".format(cabassa_checking.kind))
+
+cabassa_checking.deposit(3000)
+# print("My current balance is ${}.".format(cabassa_checking.balance))
+
+cabassa_checking.withdraw(1500, 1555)
+# print("My current balance is ${}.".format(cabassa_checking.balance))
+
+cabassa_checking.withdraw(2000, 1234)
+# print("My overdraft fee is currently {}".format(cabassa_checking.overdraft_fees))
+# print("My current balance is ${}.".format(cabassa_checking.balance))
+
+cabassa_checking.change_pin(9987)
